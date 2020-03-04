c36692f57dcf6cb9838d5cdd2d890bbb4d3a28e4
Lukas Saylor <slappyhoursound@gmail.com>
2020-03-04 13:26:40 -0500

    Updates

---
+++ precache-manifest.a906fa9807cc989d4ebed2397d3717de.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f3fc6ef31246591b01f1329a6e3500",
+    "url": "/billsbandpage/index.html"
+  },
+  {
+    "revision": "4fa19be2ab2084163c35",
+    "url": "/billsbandpage/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "39952d1942f3041c682c",
+    "url": "/billsbandpage/static/css/main.15cd80b6.chunk.css"
+  },
+  {
+    "revision": "4fa19be2ab2084163c35",
+    "url": "/billsbandpage/static/js/2.2be97aa6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/billsbandpage/static/js/2.2be97aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39952d1942f3041c682c",
+    "url": "/billsbandpage/static/js/main.657caa47.chunk.js"
+  },
+  {
+    "revision": "62e9fd57b28511a4a492",
+    "url": "/billsbandpage/static/js/runtime-main.a9c9aee9.js"
+  },
+  {
+    "revision": "837a8e6e0d671a9209a7cd26e1cd9712",
+    "url": "/billsbandpage/static/media/about.837a8e6e.png"
+  },
+  {
+    "revision": "596941a3948ccb4f09e809f329157252",
+    "url": "/billsbandpage/static/media/aboutpagelogo.596941a3.png"
+  },
+  {
+    "revision": "035ae955f0cb3f084621c2bb1fead45c",
+    "url": "/billsbandpage/static/media/billslogo.035ae955.png"
+  },
+  {
+    "revision": "f3ccc3b3313310b55b4935d92f8c4a3c",
+    "url": "/billsbandpage/static/media/billstranslogo.f3ccc3b3.png"
+  },
+  {
+    "revision": "e15434ef82c2f496cf6a1d736e1106b0",
+    "url": "/billsbandpage/static/media/contact.e15434ef.png"
+  },
+  {
+    "revision": "ee776e02779ccb064ea295b8e678f896",
+    "url": "/billsbandpage/static/media/contactpagelogo.ee776e02.png"
+  },
+  {
+    "revision": "75049b2e5aa52771e5b836967379ba4b",
+    "url": "/billsbandpage/static/media/jumbobg.75049b2e.jpg"
+  },
+  {
+    "revision": "d410d11dda02154556c6e3ebaa33799c",
+    "url": "/billsbandpage/static/media/media.d410d11d.png"
+  },
+  {
+    "revision": "9505730907520d911cfc0e196f0af61f",
+    "url": "/billsbandpage/static/media/mediapagelogo.95057309.png"
+  }
+]);
